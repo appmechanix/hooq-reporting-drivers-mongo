d3bcce11e3633e6b58b092eeb83122c1540818a2
Daniel Wylie <daniel@danielwylie.me>
2015-01-06 12:56:39 +1300

    Changed driver so schema has to be passed in.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,36 @@
 var config = require('config');
 var mongoose = require('mongoose');
 
-var driver = require('../lib/index');
+// SCHEMAS
+var logSchema = mongoose.Schema({
+    UserId: String,
+    WebhookId: String,
+    Created: { type: Date, default: Date.now },
+    RemoteIp: String,
+    Method: String,
+    Size: Number,
+    UserAgent: String,
+    ContentType: String,
+    Headers: [String]
+});
+
+var Log = mongoose.model('Logs', logSchema);
+
+
+exports.checkThatErrorIsThrownWhenNoDriver = function (test) {
+    test.throws(function () {
+        driver.getData(new Date(2014, 01, 01), new Date(2014, 12, 31), function () { });
+    }, Error);
+
+    test.done();
+}
 
 exports.checkThatDataIsReturned = function (test) {
     mongoose.connect(config.Mongo.ConnectionString);
+
+    var driver = require('../lib/index');
+    driver.setup(Log);
+
     driver.getData(new Date(2014, 01, 01), new Date(2014, 12, 31), function (err, data) {
         test.ok(data);
         test.notEqual(0, data.length);
@@ -15,6 +41,10 @@ exports.checkThatDataIsReturned = function (test) {
 
 exports.checkThatDataIsReturnedForMonth = function (test) {
     mongoose.connect(config.Mongo.ConnectionString);
+
+    var driver = require('../lib/index');
+    driver.setup(Log);
+
     driver.getData(new Date(2014, 09, 01), new Date(2014, 09, 31), function (err, data) {
         test.ok(data);
         test.equal(4, data.length);
